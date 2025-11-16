7a3f5e5fa006cf2dd1228769fcbf9f8ed02d57da
Rubén D. Guerrero N. <rudargeneira@gmail.com>
2025-11-15 22:00:33 -0400

    feat: Reduce keep-alive interval to 4 minutes

--- src/keep-alive.service.ts
+++ src/keep-alive.service.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export class KeepAliveService implements OnModuleInit {
   private readonly logger = new Logger(KeepAliveService.name);
   private readonly url = 'https://canales-taurinos.onrender.com/ping'; // tu dominio
-  private readonly intervalMs = 10 * 60 * 1000; // cada 10 min
+  private readonly intervalMs = 4 * 60 * 1000; // cada 4 min
 
   onModuleInit() {
     this.logger.log('Servicio KeepAlive iniciado 🚀');
@@ -14,7 +14,7 @@ export class KeepAliveService implements OnModuleInit {
         const res = await axios.get(this.url);
         this.logger.log(`Ping exitoso: ${res.data.message}`);
       } catch (error) {
-        this.logger.error(`Error al hacer ping: ${error.message}`);
+        this.logger.error('Error al hacer ping:', error);
       }
     }, this.intervalMs);
   }
